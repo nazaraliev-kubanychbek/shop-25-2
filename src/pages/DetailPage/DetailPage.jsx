f832649c33fdfb2b7851601f7406070e35fde5c3
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2024-10-15 19:03:56 +0600

    day - 3

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,12 +1,48 @@
-
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./detail.scss";
+import { useDispatch } from "react-redux";
+import { addCart } from "../../redux/reducer";
 
 const DetailPage = () => {
-    return (
-        <div>
-            <h1>detail page</h1>
+  const params = useParams();
+  const [data, setData] = useState({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-        </div>
+  useEffect(() => {
+    axios(`https://fakestoreapi.com/products/${params.id}`).then(({ data }) =>
+      setData(data)
     );
-}
+  }, [params.id]);
+  return (
+    <div className="detail">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <img className="detail-img" src={data.image} alt="" />
+          </div>
+
+          <div className="col-6">
+            <h1 className="detail-title">{data.title}</h1>
+            <p className="detail-text">{data.description}</p>
+            <br />
+            <p className="detail-text">{data.category}</p>
+            <p className="detail-text">${data.price}</p>
+            <button className="detail-btn"
+                onClick={()=>{
+                    dispatch(addCart(data))
+                }}
+            >buy</button>
+            <button className="detail-btn" onClick={()=>{
+                navigate(-1)
+            }}>go back</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DetailPage;
